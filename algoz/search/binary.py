1624317b22f6a0be3adecd91a57c0a8d645b0f94
hilfiger1 <tommypark2001@gmail.com>
2021-10-13 14:54:47 +0900

    First commit

---
+++ algoz/search/binary.py
@@ -0,0 +1,32 @@
+from data_structures.binary_tree import BinaryTree
+
+def binary_search(target, root):
+    if root:
+        if int(root.data) == int(target):
+            return True
+
+        if int(root.data) < int(target):
+            if binary_search(target, root.right):
+                return True
+
+        if int(root.data) > int(target):
+            if binary_search(target, root.left):
+                return True
+
+def search(args):
+    bst = BinaryTree()
+
+    print("building tree...")
+    bst.create_bst_from_file(args.file)
+    print("...tree built")
+
+    print("searching tree...")
+
+    target = args.word
+
+    if binary_search(target, bst.root):
+        print("WORD FOUND")
+        return
+    else:
+        print("WORD NOT FOUND")
+        return
---
+++ algoz/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = "algoz",
+    version = "1.0.0",
+    packages = find_packages(),
+    entry_points = {"console_scripts": ["algoz = app:main"]},
+)
